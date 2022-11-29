c9d3a7dd71e8c77ca5903c563893aad4cbfff574
dhammikod <63945345+dhammikod@users.noreply.github.com>
2022-11-29 23:27:47 +0700

    update
    asdfasdf

--- app/src/main/java/com/uc/keretaapiiotafl3/MainActivity.kt
+++ app/src/main/java/com/uc/keretaapiiotafl3/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.uc.keretaapiiotafl3
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.annotation.RequiresApi
 import com.google.firebase.database.*
 import com.uc.keretaapiiotafl3.databinding.ActivityMainBinding
 
@@ -15,124 +16,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         firebasedatabase = FirebaseDatabase.getInstance()
         databasereference = firebasedatabase?.getReference("sensor")
-//        getData1()
         getData2()
 
     }
 
-    private fun getData1(){
-        databasereference?.get()?.addOnSuccessListener {
-            var sens1= it.child("satu").value
-            if (sens1 == true)
-            {
-                binding.sensor1tv.text = "Ada"
 
-            }
-           else if (sens1 == false)
-            {
-                binding.sensor1tv.text = "No"
-
-            }
-            var sens2= it.child("dua").value
-            if (sens2 == true)
-            {
-                binding.sensor2tv.text = "Ada"
-
-            }
-            else if (sens2 == false)
-            {
-                binding.sensor2tv.text = "No"
-
-            }
-
-            var sens3= it.child("tiga").value
-            if (sens3 == true)
-            {
-                binding.sensor3tv.text = "Ada"
-
-            }
-            else if (sens3 == false)
-            {
-                binding.sensor3tv.text = "No"
-
-            }
-
-            var sens4= it.child("empat").value
-            if (sens4 == true)
-            {
-                binding.sensor4tv.text = "Ada"
-
-            }
-            else if (sens4 == false)
-            {
-                binding.sensor4tv.text = "No"
-
-            }
-           if ((sens1 == true && sens2 == true) || (sens2 == true && sens3 == true) || (sens3 == true && sens4 == true)){
-               binding.statuspalangtv.text ="Terbuka"
-           }
-            else   {
-               binding.statuspalangtv.text ="Tertutup"
-            }
-
-        }
-
-
-
-
-
-    }   private fun getData2(){
+    private fun getData2(){
         databasereference?.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
               var  sens1 = snapshot.child("satu").value
                 if (sens1 == true)
                 {
-                    binding.sensor1tv.text = "Ada"
-
-                }
-                else if (sens1 == false)
-                {
+                    binding.sensor1tv.text = "ada"
+                    binding.cvSensro1.setCardBackgroundColor(getColor(R.color.teal_200))
+                }else{
                     binding.sensor1tv.text = "No"
-
+                    binding.cvSensro1.setCardBackgroundColor(getColor(R.color.white))
                 }
+
 
                 var  sens2 = snapshot.child("dua").value
                 if (sens2 == true)
                 {
                     binding.sensor2tv.text = "Ada"
-
-                }
-                else if (sens2 == false)
-                {
+                    binding.cvSensro2.setCardBackgroundColor(getColor(R.color.teal_200))
+                }else{
                     binding.sensor2tv.text = "No"
-
+                    binding.cvSensro2.setCardBackgroundColor(getColor(R.color.white))
                 }
 
                 var  sens3 = snapshot.child("tiga").value
                 if (sens3 == true)
                 {
                     binding.sensor3tv.text = "Ada"
-
-                }
-                else if (sens3 == false)
-                {
+                    binding.cvSensro3.setCardBackgroundColor(getColor(R.color.teal_200))
+                }else{
                     binding.sensor3tv.text = "No"
-
+                    binding.cvSensro3.setCardBackgroundColor(getColor(R.color.white))
                 }
+
                 var  sens4 = snapshot.child("empat").value
                 if (sens4 == true)
                 {
                     binding.sensor4tv.text = "Ada"
-
-                }
-                else if (sens4 == false)
-                {
+                    binding.cvSensro4.setCardBackgroundColor(getColor(R.color.teal_200))
+                }else{
                     binding.sensor4tv.text = "No"
-
+                    binding.cvSensro4.setCardBackgroundColor(getColor(R.color.white))
                 }
+
                 if ((sens1 == true && sens2 == true) || (sens2 == true && sens3 == true) || (sens3 == true && sens4 == true)){
                     binding.statuspalangtv.text ="Tertutup"
                 }
@@ -146,7 +82,6 @@ class MainActivity : AppCompatActivity() {
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
-
 
         })
 }}
